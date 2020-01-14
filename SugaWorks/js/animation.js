4d84c5058a2897661e8f55446f979baa5b6e7733
m0117141 <m011714144@edu.teu.ac.jp>
2020-01-14 15:19:23 +0900

    アニメーション追加

---
+++ SugaWorks/js/animation.js
@@ -0,0 +1,22 @@
+const anim = lottie.loadAnimation({
+  container: document.getElementById('anime-wrapper'),
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: 'data.json'
+});
+
+$(function () {
+  $(".mainSite").css("display", "none");
+  setTimeout(function () {
+    $('.loadAnime').fadeOut();
+  }, 2000);
+});
+
+$(function () {
+  $(".mainSite").css({opacity: '0'});
+  setTimeout(function () {
+    $(".mainSite").css("display", "block");
+    $(".mainSite").stop().animate({opacity: '1'}, 1000);
+  }, 2500);
+});
